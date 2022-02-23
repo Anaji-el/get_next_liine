fa6cb95fda7670db94c4dec835082b2be3ee4db1
ayoub naji el idrissi <93100719+Anaji-el@users.noreply.github.com>
2022-02-23 21:24:43 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 19:41:26 by anaji-el          #+#    #+#             */
+/*   Updated: 2022/02/22 23:18:33 by anaji-el         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*ft_check(char *sv);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(const char *str);
+int		ft_strsh(char *str);
+char	*readl(char *save, int fd);
+char	*ft_rest(char *rst);
+
+#endif
